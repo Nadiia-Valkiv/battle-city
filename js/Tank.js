067b515570c56591ff05e793a49f1a9e5a257092
Nadiia Valkiv <nadiiavalkiv@coherentsolutions.com>
2022-03-09 09:36:31 +0200

    player tank move

--- js/Tank.js
+++ js/Tank.js
@@ -1,99 +1,92 @@
-import { cellSize, map} from "./map.js";
-let previousState = 'up';
+import { cellSize, map } from "./map.js";
 
 export default class Tank {
-  constructor(x, y) {
-    this.elem = document.createElement("div");
-    this.x = x;
-    this.y = y;
-    this.mapRow = this.x / cellSize;
-    this.mapColumn = this.y / cellSize;
-
-    this.directionSet = ["right", "left", "up", "down"];
-    this.direction = "up";
-    this.update();
-  }
-
-  update() {
-    this.elem.style["top"] = `${this.y}px`;
-    this.elem.style["left"] = `${this.x}px`;
-  }
-
-  randomDirection() {
-    return this.directionSet[
-      Math.floor(Math.random() * this.directionSet.length)
-    ];
-  }
-
-  move() {
-    if (Math.random() < 0.2) {
-      this.direction = this.randomDirection();
-    } else {
-        this.direction = previousState;
+    constructor(x, y, mark) {
+        this.elem = document.createElement("div");
+        this.x = x;
+        this.y = y;
+        this.mapRow = this.x / cellSize;
+        this.mapColumn = this.y / cellSize;
+        this.direction = "up";
+        this.previousState = "up";
+        this.mark = mark;
     }
 
-    switch (this.direction) {
-      case "up":
-        if (
-         map[this.mapColumn - 1] !== undefined  &&
-          map[this.mapColumn - 1][this.mapRow] === 0
-        ) {
-          map[this.mapColumn - 1][this.mapRow] = 1;
-          map[this.mapColumn][this.mapRow] = 0;
-          this.rotateTank(0);
-        } else {
-          this.move();
-        }
-        break;
+    move() {
+        switch (this.direction) {
+            case "up":
+                this.rotateTank(0);
+                if (
+                    map[this.mapColumn - 1] !== undefined &&
+                    map[this.mapColumn - 1][this.mapRow] === 0
+                ) {
+                    map[this.mapColumn - 1][this.mapRow] = this.mark;
+                    map[this.mapColumn][this.mapRow] = 0;
+                    this.y -= cellSize;
+                    this.mapColumn -= 1;
+                } else {
+                    this.changeDirection();
+                }
+                break;
 
-      case "down":
-        if (
-            map[this.mapColumn + 1] !== undefined   &&
-          map[this.mapColumn + 1][this.mapRow] === 0 
-        ) {
-          map[this.mapColumn + 1][this.mapRow] = 1;
-          map[this.mapColumn][this.mapRow] = 0;
-          this.rotateTank(180);
-        } else {
-          this.move();
-        }
+            case "down":
+                this.rotateTank(180);
+                if (
+                    map[this.mapColumn + 1] !== undefined &&
+                    map[this.mapColumn + 1][this.mapRow] === 0
+                ) {
+                    map[this.mapColumn + 1][this.mapRow] = this.mark;
+                    map[this.mapColumn][this.mapRow] = 0;
+                    this.y += cellSize;
+                    this.mapColumn += 1;
+                } else {
+                    this.changeDirection();
+                }
 
-        break;
+                break;
 
-      case "left":
-        if (
-             map[this.mapRow - 1] !== undefined &&
-          map[this.mapColumn][this.mapRow - 1] === 0 
-        ) {
-          map[this.mapColumn][this.mapRow - 1] = 1;
-          map[this.mapColumn][this.mapRow] = 0;
-          this.rotateTank(270);
-        } else {
-          this.move();
-        }
-        break;
+            case "left":
+                this.rotateTank(270);
+                if (
+                    map[this.mapColumn][this.mapRow - 1] !== undefined &&
+                    map[this.mapColumn][this.mapRow - 1] === 0
+                ) {
+                    map[this.mapColumn][this.mapRow - 1] = this.mark;
+                    map[this.mapColumn][this.mapRow] = 0;
+                    this.x -= cellSize;
+                    this.mapRow -= 1;
+                } else {
+                    this.changeDirection();
+                }
+                break;
 
-      case "right":
-        if (
-            map[this.mapRow + 1] !== undefined &&
-          map[this.mapColumn][this.mapRow + 1] === 0
-        ) {
-          map[this.mapColumn][this.mapRow + 1] = 1;
-          map[this.mapColumn][this.mapRow] = 0;
-          this.rotateTank(90);
-        } else {
-          this.move();
+            case "right":
+                this.rotateTank(90);
+                if (
+                    map[this.mapColumn][this.mapRow + 1] !== undefined &&
+                    map[this.mapColumn][this.mapRow + 1] === 0
+                ) {
+                    map[this.mapColumn][this.mapRow + 1] = this.mark;
+                    map[this.mapColumn][this.mapRow] = 0;
+                    this.x += cellSize;
+                    this.mapRow += 1;
+                } else {
+                    this.changeDirection();
+                }
+                break;
+            default:
+                console.log("wrong direction");
         }
-        break;
-      default:
-        console.log("wrong direction");
+        this.previousState = this.direction;
+        this.update();
     }
-    previousState = this.direction;
-    
-  }
-  rotateTank(degrees) {
-    const tank = document.getElementsByClassName("player")[0];
-    console.log(this.elem);
-    tank.style.transform = `rotate(${degrees}deg)`;
-  }
+
+    update() {
+        this.elem.style["top"] = `${this.y}px`;
+        this.elem.style["left"] = `${this.x}px`;
+    }
+
+    rotateTank(degrees) {
+        this.elem.style.transform = `rotate(${degrees}deg)`;
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,77 +1,85 @@
 import { map, mapLegend, cellSize } from "./map.js";
 import PlayerTank from "./PlayerTank.js";
 import EnemyTank from "./EnemyTank.js";
-const gameTimerInterval = 500;
+const gameTimerInterval = 1000;
 let playerLifeCount = 3;
 let enemyTanksCount = 21;
 let isGameOver = false;
+let isPlayerTankMove = false;
 let playerTank;
 const enemyTanks = [];
 
-/**
- * в этой функции можно выполнить весь тот код, который необходим для старта игры
- * например, именно в этом месте можно нарисовать блоки стен на карте и подписаться на события нажатия кнопок управления
- */
 gameInitialization();
 
-/**
- * Жизненный цикл игры
- * вызывает функцию gameLoop каждые gameTimerInterval до тех пор, пока игра не закончится
- * (чтобы закончить игру, установите занчение переменной isGameOver в true)
- */
-gameLoop();
+setTimeout(function () {
+    gameLoop();
+}, gameTimerInterval);
 
 function gameInitialization() {
-  let gameMap = document.querySelector("#game-map");
+    let gameMap = document.querySelector("#game-map");
 
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j] === mapLegend.wall) {
-        gameMap.innerHTML += `<div class='game-object game-object__wall' 
+    enemyTanks.splice(0, enemyTanks.length);
+
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            if (map[i][j] === mapLegend.wall) {
+                gameMap.innerHTML += `<div class='game-object game-object__wall' 
             style='position: absolute; top: ${i * cellSize}px; left: ${
-          j * cellSize
-        }px;'></div>`;
-      } else if (map[i][j] === mapLegend.playerBase) {
-        playerTank = new PlayerTank(j * cellSize, i * cellSize);
-        gameMap.appendChild(playerTank.elem);
-      } else if (map[i][j] === mapLegend.enemyBase) {
-        enemyTanks.push(new EnemyTank(j * cellSize, i * cellSize));
-
-        gameMap.appendChild(enemyTanks[enemyTanks.length - 1].elem);
-      }
+                    j * cellSize
+                }px;'></div>`;
+            } else if (map[i][j] === mapLegend.playerBase) {
+                playerTank = new PlayerTank(j * cellSize, i * cellSize);
+            } else if (map[i][j] === mapLegend.enemyBase) {
+                enemyTanks.push(new EnemyTank(j * cellSize, i * cellSize));
+            }
+        }
     }
-  }
+    gameMap.appendChild(playerTank.elem);
+    playerTank.update();
+    enemyTanks.forEach((tank) => {
+        gameMap.appendChild(tank.elem);
+    });
+    enemyTanks.forEach((tank) => {
+        tank.update();
+    });
+
+    document.addEventListener(
+        "keydown",
+        function (event) {
+            playerTank.changeDirection(event);
+            isPlayerTankMove = true;
+        },
+        false
+    );
 }
 
 function gameLoop() {
-  if (isGameOver !== true) {
-    let gameMap = document.querySelector("#game-map");
-    gameMap.innerHTML = "";
-    // playerTank.move();
-    gameInitialization();
-    playerTank.move();
-    /**
-     * вот именно в функции gameStep стоит разместить код, который будет выполняться на каждом шаге игрового цикла
-     */
-    gameStep();
+    if (isGameOver !== true) {
+        gameStep();
 
-    setTimeout(function () {
-      gameLoop();
-    }, gameTimerInterval);
-  }
+        setTimeout(function () {
+            gameLoop();
+        }, gameTimerInterval);
+    }
 }
 
 function gameStep() {
-  /**
-   * это то самое место, где стоит делать основные шаги игрового цикла
-   * например, как нам кажется, можно было бы сделать следующее
-   * 1. передвинуть пули
-   * 2. рассчитать, где танки окажутся после этого шага
-   * 3. проверить столкновения (пуль с танками, пуль со стенами, танков со стенами и танков с танками)
-   * 4. убрать с поля мертвые танки и разрушенные стены
-   * 5. проверить, не закончились ли жизни у игрока или не закончиличь ли танки противника
-   * 6. создать новые танки на базах в случае, если кого-то убили на этом шаге
-   */
+    enemyTanks.forEach((tank) => {
+        tank.move();
+    });
+    if (isPlayerTankMove) {
+        playerTank.move();
+        isPlayerTankMove = false;
+    }
+    // playerTank.move();
+    /**
+     * это то самое место, где стоит делать основные шаги игрового цикла
+     * например, как нам кажется, можно было бы сделать следующее
+     * 1. передвинуть пули
+     * 2. рассчитать, где танки окажутся после этого шага
+     * 3. проверить столкновения (пуль с танками, пуль со стенами, танков со стенами и танков с танками)
+     * 4. убрать с поля мертвые танки и разрушенные стены
+     * 5. проверить, не закончились ли жизни у игрока или не закончиличь ли танки противника
+     * 6. создать новые танки на базах в случае, если кого-то убили на этом шаге
+     */
 }
-
-
